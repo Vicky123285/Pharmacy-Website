516df56ba37d299e71207838721f70722e935f67
VICKY <vickykushwaha31971@gmail.com>
2025-02-22 10:37:15 +0530

    Update button.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+
+export function Button({ children, className, ...props }) {
+  return (
+    <button
+      className={`bg-blue-500 text-white p-2 rounded ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
 
