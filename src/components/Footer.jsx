390f2147550a33617329fd0ee8b7cbfcb92ffc6d
VICKY <vickykushwaha31971@gmail.com>
2025-02-22 10:37:58 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center mt-10">
+      <p>&copy; {new Date().getFullYear()} Pharmacy Store. All rights reserved.</p>
+    </footer>
+  );
+}
 
